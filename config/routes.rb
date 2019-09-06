65739e68791f9d3a060de9d764e7f7d24be4bcac
Alex Smirnov <sas2job@gmail.com>
2019-09-06 15:37:09 +0300

    Updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "events#index"
+
+  resources :events
+  resources :users, only: [:show, :edit, :update]
 end
