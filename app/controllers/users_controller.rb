7f45c40f7695ba6aa311c6b342af54910452526f
Alex Smirnov <sas2job@gmail.com>
2019-09-06 16:18:46 +0300

    Updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,14 @@
+# Контроллер, управляющий пользователями
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  # GET /users
-  def index
-    @users = User.all
-  end
+  # Задаем объект @user для тех действий, где он нужен
+  before_action :set_user, only: [:show, :edit, :update]
 
   # GET /users/1
   def show
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
   # GET /users/1/edit
   def edit
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
-    else
-      render :new
-    end
   end
 
   # PATCH/PUT /users/1
@@ -39,20 +20,14 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
+    # Пропишем, что разрешено передавать в params
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:name, :email)
     end
 end
