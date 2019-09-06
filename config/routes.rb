304a8b49ce14226a3617b10bc79da6379ec512bb
Alex Smirnov <sas2job@gmail.com>
2019-09-06 16:10:23 +0300

    Added comments for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events
+
+  # Поддерживаются только: show, edit, update
   resources :users, only: [:show, :edit, :update]
 end
