5f530ba0d42e6ad8fd837ad3baab7202887d9200
Alex Smirnov <sas2job@gmail.com>
2019-09-06 15:44:42 +0300

    Updated events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -46,13 +46,12 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def event_params
-      params.fetch(:event, {})
+      params.require(:event).permit(:title, :address, :datetime, :description)
     end
 end
