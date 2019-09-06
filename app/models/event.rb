e84e1e67ba1b0e5629e072df8239f4c3729ad664
Alex Smirnov <sas2job@gmail.com>
2019-09-06 15:13:37 +0300

    Added model event

---
+++ app/models/event.rb
@@ -0,0 +1,8 @@
+# Модель событие
+class Event < ApplicationRecord
+  # Валидируем заголовок, он не может быть длиннее 255 букв
+  validates :title, presence: true, length: {maximum: 255}
+  # У события должны быть заполнены место и время
+  validates :address, presence: true
+  validates :datetime, presence: true
+end
