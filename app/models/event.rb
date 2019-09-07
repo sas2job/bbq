ee6e2b189d2dbf3672a46c811df9200fa0861379
Alex Smirnov <sas2job@gmail.com>
2019-09-07 13:11:09 +0300

    Added relationship between user and events models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 # Модель событие
 class Event < ApplicationRecord
+  # Событие принадлежит юзеру
+  belongs_to :user
+
   # Валидируем заголовок, он не может быть длиннее 255 букв
   validates :title, presence: true, length: {maximum: 255}
   # У события должны быть заполнены место и время
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# Модель пользователя
 class User < ApplicationRecord
+  # Пользователь может создавать много событий
+  has_many :events
+
+  # У пользователя не должно быть имя длиннее 35 букв
+  validates :name, presence: true, length: {maximum: 35}
+
+  # У пользователя должен быть уникальный email по заданному шаблону не длиннее 255
+  # символов
+  validates :email, presence: true, length: {maximum: 255}
+  validates :email, uniqueness: true
+  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 end
