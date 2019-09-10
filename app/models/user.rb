f6b07cd8374343480d582242c4692228762c6db8
Alex Smirnov <sas2job@gmail.com>
2019-09-10 22:30:42 +0300

    Installed a gem in a project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # Модель пользователя
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # Пользователь может создавать много событий
   has_many :events
 
