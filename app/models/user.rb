7eac0701982a25cf4ecf68455729c541a01cf057
Alex Smirnov <sas2job@gmail.com>
2019-09-11 15:24:16 +0300

    Added a method set_name for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,15 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255}
   # validates :email, uniqueness: true
   # validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
+  # При создании нового юзера (create), перед валидацией объекта выполнить
+  # метод set_name
+  before_validation :set_name, on: :create
+
+  private
+
+  # Задаем юзеру случайное имя, если оно пустое
+  def set_name
+    self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+  end
 end
