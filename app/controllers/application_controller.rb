2f79286e7265324ae949412c95b20cbb6cf3d5c7
Alex Smirnov <sas2job@gmail.com>
2019-09-11 15:01:23 +0300

    Customized devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # Настройка для работы Девайза, когда юзер правит профиль
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Настройка для девайза — разрешаем обновлять профиль, но обрезаем
+  # параметры, связанные со сменой пароля.
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:password, :password_confirmation, :current_password]
+    )
+  end
 end
