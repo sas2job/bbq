fa68fc2bf633fa81d18f64aa9404d203ed2f6e88
Alex Smirnov <sas2job@gmail.com>
2019-09-12 14:09:59 +0300

    Added condition that only the user who created the event can change the event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Настройка для работы Девайза, когда юзер правит профиль
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Хелпер метод, доступный во вьюхах
+  helper_method :current_user_can_edit?
+
   # Настройка для девайза — разрешаем обновлять профиль, но обрезаем
   # параметры, связанные со сменой пароля.
   def configure_permitted_parameters
@@ -9,5 +12,11 @@ class ApplicationController < ActionController::Base
       :account_update,
       keys: [:password, :password_confirmation, :current_password]
     )
+  end
+
+  # Вспомогательный метод, возвращает true, если текущий залогиненный юзер
+  # может править указанное событие
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
   end
 end
