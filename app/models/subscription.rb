a4e1bbbc3ad5bf1da4e8587b2e1b974b11027466
Alex Smirnov <sas2job@gmail.com>
2019-09-18 09:37:07 +0300

    Update views, model for subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -12,11 +12,11 @@ class Subscription < ApplicationRecord
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
-  # для данного event_id один юзер может подписаться только один раз (если юзер задан)
-  validates :user, uniqueness: {scope: :event_id}, if: 'user.present?'
+  # Для конкретного event_id один юзер может подписаться только один раз (если юзер задан)
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
 
-  # для данного event_id один email может использоваться только один раз (если нет юзера, анонимная подписка)
-  validates :user_email, uniqueness: {scope: :event_id}, unless: 'user.present?'
+  # Или один email может использоваться только один раз (если анонимная подписка)
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
   # переопределяем метод, если есть юзер, выдаем его имя,
   # если нет -- дергаем исходный переопределенный метод
--- db/migrate/20190916172317_create_subscriptions.rb
+++ db/migrate/20190916172317_create_subscriptions.rb
@@ -1,12 +1,12 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
-      t.string, :user_name
-      t.string, :user_email
-      t.references, :event
-      t.references :user, foreign_key: true
+      t.string :user_name
+      t.string :user_email
+      t.references :event, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
