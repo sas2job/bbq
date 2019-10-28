a53def616ed18efa6e285d74de51acd76fd4750a
Alexander Smirnov <sas2job@gmail.com>
2019-10-28 13:20:39 +0300

    Fix comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -54,11 +54,11 @@ class CommentsController < ApplicationController
   end
 
   def notify_subscribers(event, comment)
-  # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-  all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
-  # По адресам из этого массива делаем рассылку
-  # Как и в подписках, берём EventMailer и его метод comment с параметрами
-  # И отсылаем в том же потоке
+    # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    # По адресам из этого массива делаем рассылку
+    # Как и в подписках, берём EventMailer и его метод comment с параметрами
+    # И отсылаем в том же потоке
     all_emails.each do |mail|
       EventMailer.comment(event, comment, mail).deliver_now
     end
